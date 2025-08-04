228f89fc040e8fad11bb5df5c07c43ae85b01f53
nealchampagne <166440916+nealchampagne@users.noreply.github.com>
2025-08-04 17:46:32 -0400

    Polish app.js, package.json, and .gitignore files

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const path = require('node:path');
-
+require('dotenv').config();
 const app = express();
+const methodOverride = require('method-override');
 
-const indexRouter = require('./routes/indexRouter');
+// This tells Express to look for a query param like ?_method=DELETE
+app.use(methodOverride('_method'));
 
-const assetsPath = path.join(__dirname, 'public');
-
-app.use(express.static(assetsPath));
+const indexRouter = require('./routes/index');
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Use ejs view engine
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve('views'));
 app.set('view engine', 'ejs');
 
+// Serve static files from the public directory
+app.use(express.static(path.resolve('public')));
+
+// Routes
 app.use('/', indexRouter);
 
 // Handle errors
